8fc179978306df11f978bcb82bdd88cbb7b73166
tmmovo <101711182+tmmovo@users.noreply.github.com>
2025-08-07 23:32:05 +0800

    Add files via upload

---
+++ source/data.js
@@ -0,0 +1,10 @@
+const events = [
+  {
+    name: "北京·ICOS国际动漫节06",
+    startDate: "2025-07-12",
+    endDate: "2025-07-13",
+    location: "首都国际会展中心（新国展二期）",
+    qq: "",
+    note: "只去13号的哦"
+  }
+];
